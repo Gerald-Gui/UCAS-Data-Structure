31eee1a4678bbee481e331f584b2bf21c0875ae7
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-03-25 11:06:25 +0800

    add LinkBinaryTree in cpp: init, create and 3 traverse ways

---
+++ ADT/C++/LinkBinaryTree.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+struct BinaryTNode{
+    T val;
+    BinaryTNode<T> * left;
+    BinaryTNode<T> * right;
+};
+
+template <typename T>
+bool InitBinaryTree(BinaryTNode<T> * root, T e){
+    if(root != nullptr){
+        delete root;
+        root = nullptr;
+    }
+    root = new BinaryTNode<T>;
+    root->val = e;
+    root->left = root->right = nullptr;
+    return true;
+}
+
+void CreateBinaryTree(BinaryTNode<char> ** root){
+    char elem;
+    cin >> elem;
+    if(elem == ' ')
+        *root = NULL;
+    else{
+        *root = new BinaryTNode<char>;
+        (*root)->val = elem;
+        CreateBinaryTree(&((*root)->left));
+        CreateBinaryTree(&((*root)->right));
+    }
+}
+
+template <typename T>
+bool PreOrderTraverse(BinaryTNode<T> * root, bool (*visit)(T e)){
+    if(root == nullptr)
+        return true;
+    if(!(*visit)(root->val))
+        return false;    
+    if(!PreOrderTraverse(root->left, visit))
+        return false;
+    if(!PreOrderTraverse(root->right, visit))
+        return false;
+}
+
+template <typename T>
+bool InOrderTraverse(BinaryTNode<T> * root, bool (*visit)(T e)){
+    if(root == nullptr)
+        return true;
+    if(!InOrderTraverse(root->left, visit))
+        return false;
+    if(!(*visit)(root->val))
+        return false;
+    if(!InOrderTraverse(root->left, visit))
+        return false;
+}
+
+template <typename T>
+bool PostOrderTraverse(BinaryTNode<T> * root, bool (*visit)(T e)){
+    if(root == nullptr)
+        return true;
+    if(!PostOrderTraverse(root->left, visit))
+        return false;
+    if(!PostOrderTraverse(root->right, visit))
+        return false;
+    if(!(*visit)(root->val))
+        return false;
+}
