ac01c1a280b2ccba62101a9b492d0366fd9ae9d1
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-04-01 18:39:06 +0800

    update LinkQueue.cpp: rewrite in a more OOP way

--- ADT/C++/LinkQueue.cpp
+++ ADT/C++/LinkQueue.cpp
@@ -2,13 +2,8 @@ template <typename T>
 struct ListNode{
     T val;
     ListNode<T> * nxt;
-    ListNode(){
-        val = 0;
-        nxt = nullptr;
-    }
-    ListNode(T e): val(e) {
-        nxt = nullptr;
-    }
+    ListNode(): val(0), nxt(nullptr) {}
+    ListNode(T e): val(e), nxt(nullptr) {}
     ListNode(T e, ListNode<T> * p): val(e), nxt(p) {}
 };
 
@@ -26,7 +21,7 @@ struct LinkQueue{
     //judge whether queue Q is empty
     inline bool IsEmpty();
     //return the num of elems in queue Q
-    int QueueLength();
+    int Length();
     //return the head elem of Q with e
     bool GetHead(T & e);
     //insert new elem e to tail of Q
@@ -34,64 +29,73 @@ struct LinkQueue{
     //delete head elem of Q and return it with e
     bool DeQueue(T & e);
     //call func visit() from front to rear
-    bool QueueTraverse_L(bool (*visit)(T e));
-
-    LinkQueue(){
-        front = rear = new ListNode<T>(0, nullptr);
-    }
-    
-    ~LinkQueue(){
-        ClearQueue();
-        delete front;
-        front = rear = nullptr;
-    }
-    
-    void Clear(){
-        T e;
-        while(DeQueue(e))
-            ;
-    }
-    
-    inline bool IsEmpty(){
-        return front == rear;
-    }
-    
-    int Length(){
-        ListNode<T> * p = front;
-        int cnt;
-        for(cnt = 0; p != rear; p = p->nxt)
-            cnt++;
-        return cnt;
-    }
-    
-    bool GetHead(T & e){
-        if(IsEmpty())
-            return false;
-        e = front->nxt->val;
-        return true;
-    }
-    
-    bool EnQueue(T e){
-        rear->nxt = new ListNode<T>(e, nullptr);
-        rear = rear->nxt;
-        return true;
-    }
-
-    bool DeQueue(T & e){
-        if(front == rear)
-            return false;
-        e = front->nxt->val;
-        ListNode<T> * p = front;
-        front = front->nxt;
-        free(p);
-        p = nullptr;
-        return true;
-    }
-    
-    bool Traverse(bool (*visit)(T e)){
-        for(ListNode<T> * p = front->nxt; p != nullptr; p = p->nxt)
-            if(!(*visit)(p->val))
-                return false;
-        return true;
-    }
+    bool Traverse(bool (*visit)(T e));
 };
+
+template <typename T>
+LinkQueue<T>::LinkQueue(){
+    front = rear = new ListNode<T>(0, nullptr);
+}
+
+template <typename T>
+LinkQueue<T>::~LinkQueue(){
+    this->Clear();
+    delete front;
+    front = rear = nullptr;
+}
+
+template <typename T>
+void LinkQueue<T>::Clear(){
+    T tmp;
+    while(this->DeQueue(tmp))
+        ;
+}
+
+template <typename T>
+inline bool LinkQueue<T>::IsEmpty(){
+    return front == rear;
+}
+
+template <typename T>
+int LinkQueue<T>::Length(){
+    ListNode<T> * p = front;
+    int cnt;
+    for(cnt = 0; p != rear; p = p->nxt)
+        cnt++;
+    return cnt;
+}
+
+template <typename T>
+bool LinkQueue<T>::GetHead(T & e){
+    if(this->IsEmpty())
+        return false;
+    e = front->nxt->val;
+    return true;
+}
+
+template <typename T>
+bool LinkQueue<T>::EnQueue(T e){
+    rear->nxt = new ListNode<T>(e, nullptr);
+    rear = rear->nxt;
+    return true;
+}
+
+template <typename T>
+bool LinkQueue<T>::DeQueue(T & e){
+    if(this->IsEmpty())
+        return false;
+    e = front->nxt->val;
+    ListNode<T> * p = front;
+    front = front->nxt;
+    delete p;
+    p = nullptr;
+    return true;
+}
+
+template <typename T>
+bool LinkQueue<T>::Traverse(bool (*visit)(T e)){
+    for(ListNode<T> * p = front->nxt; p != nullptr; p = p->nxt)
+        if(!(*visit)(p->val))
+            return false;
+    return true;
+}
