76b1ae33bfc5fa1d0c77246cef2018db46ba18ef
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-04-01 16:19:37 +0800

    update SqQueue in cpp: rewrite in a more OOP way

--- ADT/C++/SqQueue.cpp
+++ ADT/C++/SqQueue.cpp
@@ -11,11 +11,11 @@ struct LinkQueue{
     //destroy queue Q
     ~LinkQueue();
     //set queue Q empty
-    void ClearQueue();
+    void Clear();
     //judge whether queue Q is empty
     inline bool IsEmpty();
     //return the num of elems in queue Q
-    inline int Length();
+    int Length();
     //return the head elem of Q with e
     bool GetHead(T & e);
     //insert new elem e to tail of Q
@@ -24,60 +24,69 @@ struct LinkQueue{
     bool DeQueue(T & e);
     //call func visit() from front to rear
     bool Traverse(bool (*visit)(T e));
-
-    LinkQueue(){
-        data = new T[max_queue_size];
-        front = rear = 0;
-    }
-
-    ~LinkQueue(){
-        delete data;
-        data = nullptr;
-        front = rear = 0;
-    }
-
-    void ClearQueue(){
-        front = rear = 0;
-    }
-    
-    inline bool IsEmpty(){
-        return front == rear;
-    }
-
-    inline int Length(){
-        return (rear - front + max_queue_size) % max_queue_size;
-    }
-
-    bool GetHead(T & e){
-        if(IsEmpty())
-            return false;
-        e = data[front];
-        return true;
-    }
-
-    bool EnQueue(T e){
-        if((rear + 1) % max_queue_size == front)
-            return false;
-        data[rear] = e;
-        rear = (rear + 1) % max_queue_size;
-        return true;
-    }
-    
-    bool DeQueue(T & e){
-        if(front == rear)
-            return false;
-        e = data[front];
-        front = (front + 1) % max_queue_size;
-        return true;
-    }
-    
-    bool Traverse(bool (*visit)(T e)){
-        for(int i = front; i != rear; i++){
-            if(!(*visit)(data[front]))
-                return false;
-            front++;
-            front %= max_queue_size;
-        }
-        return true;
-    }
 };
+
+template <typename T>
+LinkQueue<T>::LinkQueue(){
+    data = new T[max_queue_size];
+    front = rear = 0;
+}
+
+template <typename T>
+LinkQueue<T>::~LinkQueue(){
+    delete data;
+    data = nullptr;
+    front = rear = 0;
+}
+
+template <typename T>
+void LinkQueue<T>::Clear(){
+    front = rear = 0;
+}
+
+template <typename T>
+inline bool LinkQueue<T>::IsEmpty(){
+    return front == rear;
+}
+
+template <typename T>
+int LinkQueue<T>::Length(){
+    return (rear - front + max_queue_size) % max_queue_size;
+}
+
+template <typename T>
+bool LinkQueue<T>::GetHead(T & e){
+    if(LinkQueue<T>::IsEmpty())
+        return false;
+    e = data[front];
+    return true;
+}
+
+template <typename T>
+bool LinkQueue<T>::EnQueue(T e){
+    if((rear + 1) % max_queue_size == front)
+        return false;
+    data[rear] = e;
+    rear = (rear + 1) % max_queue_size;
+    return true;
+}
+
+template <typename T>
+bool LinkQueue<T>::DeQueue(T & e){
+    if(front == rear)
+        return false;
+    e = data[front];
+    front = (front + 1) % max_queue_size;
+    return true;
+}
+
+template <typename T>
+bool LinkQueue<T>::Traverse(bool (*visit)(T e)){
+    for(int i = front; i != rear; i++){
+        if(!(*visit)(data[front]))
+            return false;
+        front++;
+        front %= max_queue_size;
+    }
+    return true;
+}
