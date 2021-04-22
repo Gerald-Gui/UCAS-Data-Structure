af26b297baa5bd8820d82dd34f4a35529b2c5ca4
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-04-22 14:24:12 +0800

    update BST: rewrite some funcs

--- ADT/C++/BinarySearchTree.cpp
+++ ADT/C++/BinarySearchTree.cpp
@@ -11,23 +11,55 @@ struct BSTNode{
     BSTNode(T data = 0): val(data) {
         left = right = nullptr;
     }
-
-    //create a BST with array
-    bool BuildBST(T * arr, int len);
-    //create a BST with array
-    bool BuildBST(vector<T> vec);
-    //search target elem in BST
-    BSTNode<T> * Search(T tar);
-    //insert a new node into BST
-    bool Insert(T elem);
-    //delete this node from BST
-    bool Delete();
-    //delete a node from BST according to tar
-    bool Delete(T tar);
-    //destroy the BST
-    void Destroy();
-    //display the BST (traverse output nodes in In Order)
-    void Display();
-    //display the BST (using recursive way)
-    void rDisplay();
 };
+
+template <typename T>
+struct BSTree {
+    BSTNode<T> *root;
+
+    BSTree() {
+        root = nullptr;
+    }
+    BSTree(T *arr, int len);
+    BSTree(vector<T> vec);
+    BSTNode<T> *Search(T tar);
+    bool Insert(T elem);
+    bool Delete(T tar);
+    static BSTNode<T> *Destroy(BSTNode<T> *p);
+};
+
+template <typename T>
+BSTree<T>::BSTree(T *arr, int len) {
+    for (int i = 0; i < len; i++) {
+        this->Insert(arr[i]);
+    }
+}
+
+template <typename T>
+bool BSTree<T>::Insert(T elem) {
+    if (root == nullptr) {
+        root = new BSTNode<T>(elem);
+        return true;
+    }
+    BSTNode<T> *p = root;
+    while ((p->left != nullptr && p->val > elem) || (p->right != nullptr && p->val < elem)) {
+        p = p->val > elem ? p->left : p->right;
+    }
+    if (p->left == nullptr && p->val > elem) {
+        p->left = new BSTNode<T>(elem);
+    } else if (p->right == nullptr && p->val < elem) {
+        p->right = new BSTNode<T>(elem);
+    } else if (p->val == elem) {
+        return false;
+    }
+    return true;
+}
+
+template <typename T>
+BSTNode<T>* BSTree<T>::Search(T tar) {
+    BSTNode<T> *p = root;
+    while (p != nullptr && p->val != tar) {
+        p = p->val > tar ? p->left : p->right;
+    }
+    return p;
+}
