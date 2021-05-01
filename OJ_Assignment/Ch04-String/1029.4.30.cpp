edd08d3e859e28b7966bfc3a0f7ba5550dc48502
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-05-01 12:44:46 +0800

    update 1029

--- OJ_Assignment/Ch04-String/1029.4.30.cpp
+++ OJ_Assignment/Ch04-String/1029.4.30.cpp
@@ -2,15 +2,30 @@
 #include <string>
 using namespace std;
 
+string SubStr(string src, size_t pos, size_t len) {
+    string sub = "";
+    if (pos > src.size()) {
+        return sub;
+    }
+    auto it = src.begin() + pos;
+    while (it != src.end() && sub.size() < len) {
+        sub += *it;
+        it++;
+    }
+    return sub;
+}
+
 int main() {
     string str;
     getline(cin, str);
 
     for (auto len = str.size() - 1; len > 0; len--) {
         for (decltype(str.size()) j = 0; j + len <= str.size(); j++) {
-            string dest = str.substr(j, len);
+            //string dest = str.substr(j, len);
+            string dest = SubStr(str, j, len);
             for (decltype(str.size()) k = j + 1; k + len <= str.size(); k++) {
-                string tmp = str.substr(k, len);
+                //string tmp = str.substr(k, len);
+                string tmp = SubStr(str, k, len);
                 if (dest == tmp) {
                     cout << dest << " " << j << endl;
                     return 0;
