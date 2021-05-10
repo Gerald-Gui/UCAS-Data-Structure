2e75c6809ebdd71a3bd0157c22baedba89990dfd
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-05-10 13:58:57 +0800

    Matrix: update init, fill matrix with val.

--- ADT/C/Matrix.c
+++ ADT/C/Matrix.c
@@ -1,21 +1,24 @@
 #include <Matrix.h>
 #include <stdlib.h>
 
-matrix_t *InitMatrix(size_t m, size_t n) {
+matrix_t *InitMatrix(size_t m, size_t n, elem_t val) {
     matrix_t *mtx = malloc(sizeof(matrix_t));
     if (!mtx) {
         abort();
     }
     mtx->r = m;
     mtx->c = n;
-    mtx->base = malloc(m * sizeof(elem_t*));
-    if (!mtx->base) {
+    if ((mtx->base = malloc(m * sizeof(elem_t*))) == NULL) {
         abort();
     }
     for (size_t i = 0; i < m; i++) {
-        mtx->base[i] = malloc(n * sizeof(elem_t));
-        if (!mtx->base[i]) {
+        if ((mtx->base[i] = malloc(n * sizeof(elem_t))) == NULL) {
             abort();
+        }
+    }
+    for (size_t i = 0; i < m; i++) {
+        for (size_t j = 0; j < n; j++) {
+            mtx->base[i][j] = val;
         }
     }
     return mtx;
--- ADT/C/Matrix.h
+++ ADT/C/Matrix.h
@@ -4,12 +4,12 @@
 #include <stddef.h>
 
 typedef int elem_t;
-typedef struct matrix {
+typedef struct {
     size_t r, c;
     elem_t **base;
 } matrix_t;
 
-matrix_t *InitMatrix(size_t m, size_t n);
+matrix_t *InitMatrix(size_t m, size_t n, elem_t val);
 matrix_t *DestroyMatrix(matrix_t *mtx);
 
 #endif
