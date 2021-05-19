b81a79d53b78993977530843ffca7012458c08c8
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-05-19 19:30:18 +0800

    fgetline: rename mygetline and support read from chosen stream

--- OJ_Assignment/fgetline.c
+++ OJ_Assignment/fgetline.c
@@ -3,17 +3,17 @@
 // ignore \n, ungetc EOF
 // if read file end, return NULL
 // otherwise return ptr line
-char *mygetline(char *line, size_t lim) {
+char *fgetline(char *line, size_t lim, FILE *stream) {
     size_t i = 0;
     char c;
-    while (--lim > 0 && (c = getchar()) != EOF && c != '\n') {
+    while (--lim > 0 && (c = fgetc(stream)) != EOF && c != '\n') {
         line[i++] = c;
     }
     line[i] = '\0';
     if (c == EOF && i == 0) {
         return NULL;
     } else if (c == EOF && i != 0) {
-        ungetc(c, stdin);
+        ungetc(c, stream);
         return line;
     } else {
         return line;
