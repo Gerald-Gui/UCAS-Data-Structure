05b6a65ab32a42eaf90bc89fb028139e5d262dbc
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-05-22 12:40:04 +0800

    fgetline.c: just reduce lines

--- OJ_Assignment/fgetline.c
+++ OJ_Assignment/fgetline.c
@@ -2,21 +2,12 @@
 
 // ignore \n, ungetc EOF
 // if read file end, return NULL
-// otherwise return the tail ptr of the string
-// you can use the return value to calculate strlen easily
+// otherwise return line or p
+// adjust the return value according to your wish
 char *fgetline(char *line, size_t lim, FILE *stream) {
-    char c;
-    char *p = NULL;
-    for (p = line; --lim > 0 && (c = getchar()) != EOF && c != '\n'; p++) {
-        *p = c;
-    }
+    char c, *p = NULL;
+    for (p = line; --lim > 0 && (c = getchar()) != EOF && c != '\n'; *(p++) = c) {}
     *p = '\0';
-    if (c == EOF && p == line) {
-        return NULL;
-    } else if (c == EOF && p != line) {
-        ungetc(c, stream);
-        return p;
-    } else {
-        return p;
-    }
+    if (c == EOF && p != line) ungetc(c, stream);
+    return c == EOF && p == line ? NULL : p;
 }
