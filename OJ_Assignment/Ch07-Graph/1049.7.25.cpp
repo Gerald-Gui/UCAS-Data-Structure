b0897ff65fc1aebd551ed77ed6b49f6a343a4dc8
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-06-02 14:32:08 +0800

    update 1049

--- OJ_Assignment/Ch07-Graph/1049.7.25.cpp
+++ OJ_Assignment/Ch07-Graph/1049.7.25.cpp
@@ -1,91 +1,74 @@
-#include <cstdlib>
 #include <cstdio>
 #include <cstring>
-#include <algorithm>
-using namespace std;
+#include <cstdlib>
+#include <vector>
 
-#define ARR_INIT_SIZE 8
-#define STR_LEN 1000
+#define STRLEN 1000
 
-char *fgetline(char *line, size_t lim, FILE *stream) {
-    char c, *p = NULL;
-    for (p = line; --lim > 0 && (c = fgetc(stream)) != EOF && c != '\n'; *(p++) = c) {}
-    *p = '\0';
-    if (c == EOF && p != line) ungetc(c, stream);
-    return c == EOF && p == line ? NULL : line;
-}
-
-size_t *BuildDynamicArray(char *str, size_t *arr) {
-    if (arr != nullptr) {
-        free(arr);
-    }
-    arr = (size_t*)malloc(ARR_INIT_SIZE * sizeof(size_t));
-    size_t arrsize = ARR_INIT_SIZE;
-    char *p = strtok(str, " ");
-    size_t i = 0;
-    arr[i++] = atoi(p);
-    while ((p = strtok(nullptr, " ")) != nullptr) {
-        if (i >= arrsize) {
-            arrsize <<= 1;
-            arr = (size_t*)realloc(arr, arrsize);
-        }
-        arr[i++] = atoi(p);
-    }
-    return arr;
-}
-
-void Str2Arr(char *str, size_t *arr) {
-    char *p = strtok(str, " ");
-    size_t i = 0;
-    arr[i++] = atoi(p);
-    while ((p = strtok(nullptr, " ")) != nullptr) {
-        arr[i++] = atoi(p);
-    }
-}
-
-bool Cycle(const size_t i, const size_t *s, const size_t *fst, const size_t *lst, bool *visited, bool *visited_tmp) {
-    visited[i] = true;
-    if (visited_tmp[i]) {
-        return true;
-    }
-    visited_tmp[i] = true;
-    for (size_t j = fst[i]; j < lst[i]; j++) {
-        if (Cycle(s[j], s, fst, lst, visited, visited_tmp)) {
-            return true;
-        }
-    }
-    visited_tmp[i] = false;
-    return false;
-}
+char *fgetline(char *line, size_t lim, FILE *stream);
+size_t Line2Vector(char *line, std::vector<int> &vec);
+bool Cycle(int v, const std::vector<int> &s, const int *fst, const int *lst, int *visited);
 
 int main() {
-    size_t n;
-    char line[STR_LEN];
-    n = atoi(fgetline(line, STR_LEN, stdin));
+    char line[STRLEN];
+    int n = atoi(fgetline(line, STRLEN, stdin));
 
-    size_t *s = nullptr;
-    size_t *fst = (size_t*)malloc((n + 1) * sizeof(size_t));
-    size_t *lst = (size_t*)malloc((n + 1) * sizeof(size_t));
-    s = BuildDynamicArray(fgetline(line, STR_LEN, stdin), s);
-    
-    Str2Arr(fgetline(line, STR_LEN, stdin), fst);
-    Str2Arr(fgetline(line, STR_LEN, stdin), lst);
+    std::vector<int> s;
+    Line2Vector(fgetline(line, STRLEN, stdin), s);
 
-    bool *visited = (bool*)malloc((n + 1) * sizeof(bool));
-    bool *visited_tmp = (bool*)malloc((n + 1) * sizeof(bool));
-    for (size_t i = 0; i < n + 1; i++) {
-        visited[i] = visited_tmp[i] = false;
+    int *fst = new int[n + 1];
+    int *lst = new int[n + 1];
+    fst[0] = lst[0] = 0;
+    int i = 0;
+    for (char *p = strtok(fgetline(line, STRLEN, stdin), " "); p != nullptr; p = strtok(nullptr, " ")) {
+        fst[i++] = atoi(p);
+    }
+    i = 0;
+    for (char *p = strtok(fgetline(line, STRLEN, stdin), " "); p != nullptr; p = strtok(nullptr, " ")) {
+        lst[i++] = atoi(p);
     }
 
+    int *visited = new int[n + 1];
+    memset(visited, 0, (n + 1) * sizeof(int));
+
     bool flg = false;
-    for (size_t i = 1; i <= n; i++) {
-        if (!visited[i] && Cycle(i, s, fst, lst, visited, visited_tmp)) {
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i] && Cycle(i, s, fst, lst, visited)) {
             flg = true;
             break;
         }
     }
     printf("%s\n", flg ? "yes" : "no");
-    
+
     return 0;
 }
 
+char *fgetline(char *line, size_t lim, FILE *stream) {
+    char c, *p = nullptr;
+    for (p = line; --lim > 0 && (c = fgetc(stream)) != EOF && c != '\n'; *(p++) = c) {}
+    *p = '\0';
+    if (c == EOF && p != line) ungetc(c, stream);
+    return c == EOF && p == line ? nullptr : line;
+}
+
+size_t Line2Vector(char *line, std::vector<int> &vec) {
+    vec.clear();
+    for (char *p = strtok(line, " "); p != nullptr; p = strtok(nullptr, " ")) {
+        vec.push_back(atoi(p));
+    }
+    return vec.size();
+}
+
+bool Cycle(int v, const std::vector<int> &s, const int *fst, const int *lst, int *visited) {
+    if (visited[v] == -1) {
+        return true;
+    }
+    visited[v] = -1;
+    for (int i = fst[v]; i < lst[v]; i++) {
+        if (Cycle(s[i], s, fst, lst, visited)) {
+            return true;
+        }
+    }
+    visited[v] = 1;
+    return false;
+}
