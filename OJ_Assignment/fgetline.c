275d22927805a7e69131964fcbd31a946e4c9732
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-06-16 16:53:49 +0800

    fgetline.c: rm unnecessary brakets, adjust ret val

--- OJ_Assignment/fgetline.c
+++ OJ_Assignment/fgetline.c
@@ -6,8 +6,8 @@
 // adjust the return value according to your wish
 char *fgetline(char *line, size_t lim, FILE *stream) {
     char c, *p = NULL;
-    for (p = line; --lim > 0 && (c = fgetc(stream)) != EOF && c != '\n'; *(p++) = c) {}
+    for (p = line; --lim > 0 && (c = fgetc(stream)) != EOF && c != '\n'; *p++ = c) {}
     *p = '\0';
     if (c == EOF && p != line) ungetc(c, stream);
-    return c == EOF && p == line ? NULL : p;
+    return c == EOF && p == line ? NULL : line;
 }
